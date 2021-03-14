1da5c59d4fc4d25059f92bd9c45ddf8c29a00038
Andrej Hosna <hosna.andrej@gmail.com>
2021-03-14 20:40:13 +0000

    Import

---
+++ Calculator/Calculator/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDodaj_Click(object sender, EventArgs e)
+        {
+            decimal pierwsza = nUDPierwsza.Value;
+            decimal druga = nUDDruga.Value;
+            decimal wynik = pierwsza + druga;
+            txtWynik.Text = wynik.ToString();
+        }
+
+        private void btnOdejmij_Click(object sender, EventArgs e)
+        {
+            decimal pierwsza = nUDPierwsza.Value;
+            decimal druga = nUDDruga.Value;
+            decimal wynik = pierwsza - druga;
+            txtWynik.Text = wynik.ToString();
+        }
+
+        private void btnPomnoz_Click(object sender, EventArgs e)
+        {
+            decimal pierwsza = nUDPierwsza.Value;
+            decimal druga = nUDDruga.Value;
+            decimal wynik = pierwsza * druga;
+            txtWynik.Text = wynik.ToString();
+        }
+
+        private void btnPodziel_Click(object sender, EventArgs e)
+        {
+            decimal pierwsza = nUDPierwsza.Value;
+            decimal druga = nUDDruga.Value;
+            if (druga != 0)
+            {
+                decimal wynik = pierwsza/druga;
+                txtWynik.Text = wynik.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Nie dziel przez zero!");
+            }
+        }
+    }
+}
